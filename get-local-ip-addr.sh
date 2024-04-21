c83c843a9ec66e6909adc2a83046840bf2bcb294
Ko001 <syapen4bkoji@gmail.com>
2024-04-21 22:49:40 +0900

    docker 関係

--- get-local-ip-addr.sh
+++ get-local-ip-addr.sh
@@ -1,4 +1,4 @@
 IP=`ifconfig | grep inet | cut -d " " -f2 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | awk 'NR==2'`
-sed -i".bak" -e "/REACT_NATIVE_PACKAGER_HOSTNAME=/d" myapp/.env
-echo "REACT_NATIVE_PACKAGER_HOSTNAME=$IP" >> myapp/.env
+sed -i".bak" -e "/REACT_NATIVE_PACKAGER_HOSTNAME=/d" HimaApp/.env
+echo "REACT_NATIVE_PACKAGER_HOSTNAME=$IP" >> HimaApp/.env
 echo "env にローカルIPが設定されました。"
